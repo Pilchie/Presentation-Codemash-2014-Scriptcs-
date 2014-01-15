1bcfb2123de8ccedc6bfb2a858f51efd9b2ab9d3
Kevin Pilch-Bisson <kevin@pilch-bisson.net>
2014-01-14 20:40:47 -0800

    Add ScriptCs hosting via Config-R.

---
+++ ConfigRDemo/ConfigRDemo/ConfigRDemo.exe.csx
@@ -0,0 +1,19 @@
+﻿Add("Number", 42L);
+Add("Uri", new Uri("http://codemash.org"));
+
+#region Fun
+long Fib(int ordinal)
+{
+    if (ordinal <= 0)
+    {
+        throw new ArgumentException();
+    }
+
+    if (ordinal == 1 || ordinal == 2)
+    {
+        return 1;
+    }
+
+    return Fib(ordinal - 1) + Fib(ordinal - 2);
+}
+#endregion
---
+++ ConfigRDemo/ConfigRDemo/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using ConfigR;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var count = Config.Global.Get<long>("Number");
+        var uri = Config.Global.Get<Uri>("Uri");
+
+        Console.WriteLine(
+            "Got {0} and {1} from the configuration",
+            count,
+            uri);
+    }
+}
